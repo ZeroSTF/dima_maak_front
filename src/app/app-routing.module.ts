d9023c0af0884b4f0d6165ed860a784490291779
ZeroSTF <kateskatous@gmail.com>
2024-04-12 18:39:56 +0100

    -Added necessary components -Changed favicon to be Dima Maak icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AllTemplateFrontComponent} from "./FrontOffice/all-template-front/all-template-front.component";
+import {AllTemplateBackComponent} from "./BackOffice/all-template-back/all-template-back.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:AllTemplateFrontComponent
+  },
+  {
+    path:"admin",
+    component:AllTemplateBackComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterBackComponent,
+    NavbarBackComponent,
+    SidebarBackComponent,
+    AllTemplateBackComponent,
+    AllTemplateFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent
   ],
   imports: [
     BrowserModule,
