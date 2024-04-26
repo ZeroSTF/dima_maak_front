3a04f7b7b67d6b12f5a4b231413857780e94ef46
ZeroSTF <kateskatous@gmail.com>
2024-04-26 12:40:45 +0100

    auth service implementation

---
+++ src/app/Service/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl:String = 'http://localhost:8080/auth/'
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  constructor(private http : HttpClient) { }
+
+  isAuthenticated() {
+    return !!localStorage.getItem('accessToken') ;
+  }
+
+  login(requestBody : {email : string , password : string}) {
+    return this.http.post(this.baseUrl+`login`,requestBody,{ headers : this.headers , withCredentials: true  })
+  }
+
+  logout() {
+    return this.http.post(this.baseUrl+`logout`,{ headers : this.headers , withCredentials: true  })
+  }
+
+  register(requestBody :any) {
+    return this.http.post(this.baseUrl+`register`,requestBody,{ headers : this.headers , withCredentials: true  })
+  }
+}
