74f75eb9b9888411c71e61f69ecf455c22f2db35
ZeroSTF <kateskatous@gmail.com>
2024-05-04 16:22:02 +0100

    front header fix?

--- src/app/FrontOffice/header-front/header-front.component.ts
+++ src/app/FrontOffice/header-front/header-front.component.ts
@@ -1,29 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../../Service/auth.service";
 import {NavigationEnd, Router} from "@angular/router";
 import {UserService} from "../../Service/user/user.service";
 import {NotificationService} from "../../Service/user/notification.service";
-import {fromEvent, interval, merge, startWith, switchMap, tap, throttleTime} from "rxjs";
+import {
+  fromEvent,
+  interval,
+  merge,
+  startWith,
+  Subject,
+  Subscription,
+  switchMap,
+  takeUntil,
+  tap,
+  throttleTime
+} from "rxjs";
 
 @Component({
   selector: 'app-header-front',
   templateUrl: './header-front.component.html',
   styleUrls: ['./header-front.component.css']
 })
-export class HeaderFrontComponent implements OnInit {
+export class HeaderFrontComponent implements OnInit, OnDestroy {
   profile: any;
   unreadNotificationsCount = 0;
-  unreadNotifications:any;
-  isDropdownOpen= false;
+  unreadNotifications: any;
+  isDropdownOpen = false;
+  stopUserActivityCheck$ = new Subject();
+  userActivitySubscription = new Subscription();
 
   constructor(public authService: AuthService, private router: Router, private userService: UserService, private notificationService: NotificationService) {
   }
 
   ngOnInit() {
-    if(this.authService.isAuthenticated()){
-    this.userService.getProfile().subscribe(data => {
-      this.processProfile(data);
-    });
+    console.log("TESTING IS AUTHENTITCATED " + this.authService.isAuthenticated());
+    if (this.authService.isAuthenticated()) {
+      console.log("I GOT IN");
+      this.userService.getProfile().subscribe(data => {
+        this.processProfile(data);
+      });
       // Define sources of user activity
       const clicks = fromEvent(document, 'click');
       const mouseMoves = fromEvent(document, 'mousemove');
@@ -36,25 +51,25 @@ export class HeaderFrontComponent implements OnInit {
       const throttledUserActivity = userActivity.pipe(throttleTime(1000));
 
       // Switch to interval when user is active, and stop when user is idle
-      throttledUserActivity.pipe(
+      this.userActivitySubscription = throttledUserActivity.pipe(
         switchMap(() => {
-          // User is active: start interval
-          return interval(60000).pipe(
-            startWith(0),  // To immediately fetch the count at the start
-            tap(() => this.getUnreadNotificationsCount())
-          );
+          return interval(5000).pipe(startWith(100), tap(() => this.getUnreadNotificationsCount()), takeUntil(this.stopUserActivityCheck$));
         })
       ).subscribe();
     }
   }
+
   processProfile(data: any) {
     this.profile = data;
   }
+
   logout() {
     this.authService.logout().subscribe(
       (data: any) => {
         localStorage.removeItem('token');
         console.log('Logout successful:', data.message);
+        this.stopUserActivityCheck$.next(1);
+        this.userActivitySubscription.unsubscribe();
         this.router.navigate(["/"]);
       },
       error => {
@@ -70,9 +85,10 @@ export class HeaderFrontComponent implements OnInit {
       return dateB.getTime() - dateA.getTime();
     });
   }
+
   timeDiff(date: Date): string {
     const now = new Date();
-    const dateNotif= new Date(date);
+    const dateNotif = new Date(date);
     const diff = now.getTime() - dateNotif.getTime();
     const seconds = Math.floor(diff / 1000);
     const minutes = Math.floor(seconds / 60);
@@ -88,13 +104,22 @@ export class HeaderFrontComponent implements OnInit {
       return 'now';
     }
   }
+
   getUnreadNotificationsCount() {
-    this.notificationService.getUnread().subscribe((data:any) => {
+    this.notificationService.getUnread().subscribe((data: any) => {
       this.unreadNotificationsCount = data.length;
-      this.unreadNotifications=data;
+      this.unreadNotifications = data;
     });
   }
+
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  ngOnDestroy() {
+    this.stopUserActivityCheck$.complete();
+    if (this.userActivitySubscription && !this.userActivitySubscription.closed) {
+      this.userActivitySubscription.unsubscribe();
+    }
   }
 }
